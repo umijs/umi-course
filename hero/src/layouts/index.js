5dff5debd49b7cb0f6cdcd949064b7db8d2a48f4
xiaohuoni <448627663@qq.com>
2018-12-05 17:30:32 +0800

    :sparkles: add layout

--- hero/src/layouts/index.js
+++ hero/src/layouts/index.js
@@ -1,11 +1,31 @@
 import styles from './index.css';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 function BasicLayout(props) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      { props.children }
-    </div>
+    <Layout>
+      <Header>
+        <div className={styles.logo}>王者荣耀资料库 </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">英雄</Menu.Item>
+          <Menu.Item key="2">局内道具</Menu.Item>
+          <Menu.Item key="3">召唤师技能</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          {props.children}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Umi 入门教程 Created by xiaohuoni</Footer>
+    </Layout>
   );
 }
 
