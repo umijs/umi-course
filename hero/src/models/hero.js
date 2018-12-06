fe8480bda23582087fa9ae7ef60d2dcd8c34da69
xiaohuoni <448627663@qq.com>
2018-12-06 22:34:13 +0800

    :sparkles: history listen pathname

--- hero/src/models/hero.js
+++ hero/src/models/hero.js
@@ -1,17 +1,52 @@
-
 export default {
-  state: 'hero',
+  state: {
+    heros: [],
+  },
   subscriptions: {
     setup({ dispatch, history }) {
+      return history.listen(({ pathname, query }) => {
+        if (pathname === '/hero') {
+          dispatch({
+            type: 'fetch',
+          });
+        }
+      });
     },
   },
   reducers: {
     update(state) {
       return `${state}_hero`;
     },
+    save(state, action) {
+      return { ...state, ...action.payload };
+    },
   },
   effects: {
     *fetch({ type, payload }, { put, call, select }) {
+      const data = [
+        {
+          ename: 105,
+          cname: '廉颇',
+          title: '正义爆轰',
+          new_type: 0,
+          hero_type: 3,
+          skin_name: '正义爆轰|地狱岩魂',
+        },
+        {
+          ename: 106,
+          cname: '小乔',
+          title: '恋之微风',
+          new_type: 0,
+          hero_type: 2,
+          skin_name: '恋之微风|万圣前夜|天鹅之梦|纯白花嫁|缤纷独角兽',
+        },
+      ];
+      yield put({
+        type: 'save',
+        payload: {
+          heros: data,
+        },
+      });
     },
   },
-}
+};
--- hero/src/pages/hero.js
+++ hero/src/pages/hero.js
@@ -4,7 +4,7 @@ function Hero(props) {        
   return (
     <div className={styles.normal}>
       <h1>Page hero</h1>
-      <h2>This is {props.hero}</h2>
+      <h2>This is {JSON.stringify(props.hero)}</h2>
     </div>
   );
 }
