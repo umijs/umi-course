47e164c0037012664d53fa3190ebf98747155765
xiaohuoni <448627663@qq.com>
2018-12-18 20:18:48 +0800

    :sparkles: use less

--- hero/src/pages/hero/index.js
+++ hero/src/pages/hero/index.js
@@ -3,20 +3,18 @@ import { connect } from 'dva';
 import { Row, Col } from 'antd';
 
 function Hero({ hero }) {
-  const { heros=[] } = hero;
+  const { heros = [] } = hero;
   return (
     <div className={styles.normal}>
       <Row>
         {heros.reverse().map(item => (
-            <Col key={item.ename} span={3} className={styles.heroitem}>
-              <img
-                src={`https://game.gtimg.cn/images/yxzj/img201606/heroimg/${item.ename}/${
-                  item.ename
-                }.jpg`}
-              />
-              <p>{item.cname}</p>
-            </Col>
-          ))}
+          <Col key={item.ename} span={3} className={styles.heroitem}>
+            <img
+              src={`https://game.gtimg.cn/images/yxzj/img201606/heroimg/${item.ename}/${item.ename}.jpg`}
+            />
+            <p>{item.cname}</p>
+          </Col>
+        ))}
       </Row>
     </div>
   );
