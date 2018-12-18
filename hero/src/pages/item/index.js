8c1fc94561d9e26cf8b1a7055ffda0bf227342d6
xiaohuoni <448627663@qq.com>
2018-12-18 20:29:23 +0800

    :sparkles:  item page

--- hero/src/pages/item/index.js
+++ hero/src/pages/item/index.js
@@ -1,11 +1,21 @@
-import styles from './index.css';
+import styles from './index.less';
 import { connect } from 'dva';
+import { Row, Col } from 'antd';
 
-function Item(props) {
+function Item({item}) {
+  const { items = [] } = item;
   return (
     <div className={styles.normal}>
-      <h1>Page item</h1>
-      <h2>This is {JSON.stringify(props.item)}</h2>
+      <Row>
+        {items.reverse().map(data => (
+          <Col key={data.item_id} span={3} className={styles.heroitem}>
+            <img
+              src={`https://game.gtimg.cn/images/yxzj/img201606/itemimg/${data.item_id}.jpg`}
+            />
+            <p>{data.item_name}</p>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
